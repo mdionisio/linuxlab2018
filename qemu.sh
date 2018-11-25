ba4ec06cc72f40354927ac7b058e04489afca14a
Michele Dionisio <michele.dionisio@gmail.com>
2018-11-25 17:45:33 +0100

    [UPG] upgrade qemu start script

--- qemu.sh
+++ qemu.sh
@@ -9,15 +9,20 @@ else
 	if [ x$1 = "xdebug" ]; then
 		extraparam="${extraparam} -s -S"
 	fi
-
-	./qemu/arm-softmmu/qemu-system-arm  ${extraparam} -m 512 -M sabrelite \
-	-kernel ./linux/arch/arm/boot/zImage \
-	-initrd initrd-test.img.gz \
-	-append "console=ttymxc0" \
-	-dtb ./linux/arch/arm/boot/dts/imx6q-sabrelite.dtb \
-	-global mytest.data1=2 \
-	-serial mon:stdio \
-	-nographic \
-	-monitor telnet:127.0.0.1:55555,server,nowait
 	
+	if [ x$1 = "xgdb" ]; then
+		cd ./linux
+		gdb-multiarch -tui -ex "target remote tcp:localhost:1234" ./vmlinux
+		cd ..
+	else
+		./qemu/arm-softmmu/qemu-system-arm  ${extraparam} -m 512 -M sabrelite \
+			-kernel ./linux/arch/arm/boot/zImage \
+			-initrd initrd-test.img.gz \
+			-append "console=ttymxc0" \
+			-dtb ./linux/arch/arm/boot/dts/imx6q-sabrelite.dtb \
+			-global mytest.data1=2 \
+			-serial mon:stdio \
+			-nographic \
+			-monitor telnet:127.0.0.1:55555,server,nowait
+	fi
 fi
