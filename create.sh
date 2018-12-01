7d15a685e47791a187ce7d45a5a5320b1f6680f4
Michele Dionisio <michele.dionisio@gmail.com>
2018-12-01 09:37:13 +0100

    [UPG] create rootfs with root privileges

--- create.sh
+++ create.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
  
 #Remove old files
-sudo rm -rf rootfs
+rm -rf rootfs 2>/dev/null >/dev/null
  
 #Create new rootfs folder
-sudo mkdir rootfs
-sudo cp busybox/_install/* rootfs/ -raf
-sudo cp linux/_install/* rootfs/ -raf
-#sudo cp mytestmodules/*.ko rootfs/lib/modules/4.18.11/kernel/drivers/
+mkdir rootfs
+cp busybox/_install/* rootfs/ -raf
+cp linux/_install/* rootfs/ -raf
+#cp mytestmodules/*.ko rootfs/lib/modules/4.18.11/kernel/drivers/
  
 #Create folders required by Linux convention
-sudo mkdir -p rootfs/proc/
-sudo mkdir -p rootfs/sys/
-sudo mkdir -p rootfs/tmp/
-sudo mkdir -p rootfs/root/
-sudo mkdir -p rootfs/var/
-sudo mkdir -p rootfs/mnt/
-sudo mkdir -p rootfs/dev/
+mkdir -p rootfs/proc/
+mkdir -p rootfs/sys/
+mkdir -p rootfs/tmp/
+mkdir -p rootfs/root/
+mkdir -p rootfs/var/
+mkdir -p rootfs/mnt/
+mkdir -p rootfs/dev/
 
-sudo cp ./init ./rootfs
+cp ./init ./rootfs
  
-rm -f initrd-test.img
-cd rootfs ; find . | cpio -o -H newc > ../initrd-test.img ; cd ..
+rm -f initrd-test.img 2>/dev/null >/dev/null
+cd rootfs ; find . | cpio -o -H newc -R +0:+0 > ../initrd-test.img ; cd ..
 rm initrd-test.img.gz
 gzip initrd-test.img
+rm -rf rootfs
