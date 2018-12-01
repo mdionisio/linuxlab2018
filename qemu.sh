7de781d9dc58c450686d1f905c488e373dedee8e
Michele Dionisio <michele.dionisio@gmail.com>
2018-12-01 09:49:05 +0100

    [UPG] more easy script to attach gdb

--- qemu.sh
+++ qemu.sh
@@ -12,7 +12,12 @@ else
 	
 	if [ x$1 = "xgdb" ]; then
 		cd ./linux
-		gdb-multiarch -tui -ex "target remote tcp:localhost:1234" ./vmlinux
+		gdb-multiarch -tui \
+			-ex "target remote tcp:localhost:1234"                                      \
+			-ex "break qemu-test.c:qemudev_irq_handler"   \
+			-ex "break qemu-test.c:qemudev_read"               \
+			-ex "break qemu-test.c:qemudev_write"             \
+			./vmlinux
 		cd ..
 	else
 		./qemu/arm-softmmu/qemu-system-arm  ${extraparam} -m 512 -M sabrelite \
